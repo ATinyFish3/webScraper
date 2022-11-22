97b8dbea7d558e8b49ae699ed785f873277cdb20
Matthew <60041166+ATinyFish3@users.noreply.github.com>
2022-11-22 15:30:06 +0000

    Add user search setup functionality

--- code/main.py
+++ code/main.py
@@ -4,21 +4,35 @@ import requests
 from bs4 import BeautifulSoup
 
 def scraper():
-    #Base URL to use, get from user input later
-    URL = "https://en.wikipedia.org/wiki/Web_scraping"
+    NUM_RESULTS = 3
+    BASE_WIKI_ADDRESS = 'https://en.wikipedia.org'
 
-    #Get page html
-    page = requests.get(URL)
+    #Get results from user search
+    wikiLinks = []
+    searchResults = userSearch("web scraping", NUM_RESULTS)
+    for i in searchResults:
+        wikiLinks.append(i.find('a')['href'])
 
-    #Get soup object
-    soup = BeautifulSoup(page.content, "html.parser")
+    #print(wikiLinks)
+
+    #need to get user to choose which result they want to use, for now just use top result
+
+    URL = BASE_WIKI_ADDRESS + wikiLinks[0]
+    print(URL)
+
 
     #Get headers from contents table || get from user input later
-    contentsHeaders = soup.find(id="toc")
+    #contentsHeaders = soup.find(id="toc")# id of contents
 
-    print(contentsHeaders.prettify())
+    #print(contentsHeaders.text)
 
-    print(contentsHeaders.text)
+def userSearch(searchTerm, numResults):
+    searchReq = "https://en.wikipedia.org/w/index.php?search=" + searchTerm + "&title=Special:Search&profile=advanced&fulltext=1&ns0=1"
+    page = requests.get(searchReq)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    allResults = soup.find_all('div', class_='mw-search-result-heading')
+    return allResults[:numResults]
 
 if __name__ == '__main__':
     scraper()
